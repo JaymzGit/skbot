7adfd408df67179bb5eec1110d5134bb5d679198
Jaymz <58385512+JaymzGit@users.noreply.github.com>
2021-04-29 14:35:14 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,3 +11,12 @@ bot.on('message', (msg) => {
       bot.deleteMessage(chatId)
     }
   })
+
+
+  const express = require('express');
+  const app = express();
+  const port = 3000;
+  
+  app.get('/', (req, res) => res.send('Hello World!'));
+  
+  app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
