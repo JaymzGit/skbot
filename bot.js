d8e64a27e8ef8fb1436627455ff919a2efe036ff
Jaymz <58385512+JaymzGit@users.noreply.github.com>
2021-05-06 15:11:44 +0800

    Update bot.js
    added "akeem" as a blacklisted word

--- bot.js
+++ bot.js
@@ -9,13 +9,17 @@ const app = express();
 const port = 3000;
 
 bot.on('message', (msg) => {
-    let chat_id = msg.chat.id;
-    let message_id = msg.message_id;
-    if (msg.text.toLowerCase().includes('dilepaskan') || msg.text.toLowerCase().includes('ansuran') || 
-    msg.text.toLowerCase().includes('myset') ||msg.text.toLowerCase().includes('64gb') || msg.text.toLowerCase().includes('mentraktirmu'))  {
+  let chat_id = msg.chat.id;
+  let message_id = msg.message_id;
+  if (msg.text.toLowerCase().includes('dilepaskan') ||        
+      msg.text.toLowerCase().includes('ansuran') || 
+      msg.text.toLowerCase().includes('myset') || 
+      msg.text.toLowerCase().includes('64gb') || 
+      msg.text.toLowerCase().includes('mentraktirmu') || 
+      msg.text.toLowerCase().includes('akeem'))  {
       bot.deleteMessage(chat_id, message_id);
-    }
-  })
+  }
+})
 
 app.get('/', (req, res) => res.send('Bot is running!'));
   
