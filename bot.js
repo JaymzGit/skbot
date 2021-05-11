546c317666d204102e3261e7f60355aefb521eab
Jaymz <58385512+JaymzGit@users.noreply.github.com>
2021-05-11 16:17:53 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,25 @@
-  const fs =  require('fs');
+//Import required modules
+const fs =  require('fs');
 var readline = require('readline');
 const TelegramBot = require('node-telegram-bot-api')
 
+//Insert token here
 const token = '';
 
 const bot = new TelegramBot(token, {polling: true});
 
+//Connect bot with web
 const express = require('express');
 const app = express();
 const port = 3000;
 
-
+//Read blacklist.txt
 function readf(){
     var array = fs.readFileSync('blacklist.txt', 'utf8').toString().replace(/\r\n/g,'\n').split('\n');
     return array;
 }
 
+//Responds to command "/add <word>"
 bot.onText(/\/add (.+)/, (msg, match) => {
     const chat_id = msg.chat.id;
     let message_id = msg.message_id;
@@ -23,6 +27,7 @@ bot.onText(/\/add (.+)/, (msg, match) => {
 		if ((data.status == "creator") || (data.status == "administrator")){
             const newword = match[1];
             
+	    //Append the file (add to the last row)
             fs.appendFile('blacklist.txt', '\r\n'+newword, function (err) {
                 if (err) throw err;
               });
@@ -43,6 +48,7 @@ bot.on('message', (msg) => {
   let message_id = msg.message_id;
   var arrayLength = arr.length;
   for (var i = 0; i < arrayLength; i++) {
+	//Read the blacklist.txt files and scan for blacklisted words in messages
     if (msg.text.toLowerCase().includes(arr[i])) {
         console.log(arr[i]);
         bot.deleteMessage(chat_id, message_id);
